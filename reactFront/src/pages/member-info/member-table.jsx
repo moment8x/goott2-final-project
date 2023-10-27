3794aa9bebc75ac60cb79fbbed74429efc532b63
kb <moment8x@gmail.com>
2023-10-28 00:03:20 +0900

    2023-10-27 admin 고객 메뉴 진행 중 (reactFront 추가)

---
+++ reactFront/src/pages/member-info/member-table.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExampleTwo from './table/SearchedMember';
+
+const SearchedMemberTable = () => {
+  return (
+    <div className=" space-y-5">
+      <SearchedMember />
+    </div>
+  );
+};
+
+export default TableAdvancedPage;
